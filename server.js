4340e26a248ef6a133246d30f0ff5732bee0a26a
Loreina Salalila <vialoraine.gg@gmail.com>
2020-12-21 10:35:32 -0800

    debugging

--- server.js
+++ server.js
@@ -3,26 +3,33 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
 app.use(logger("dev"));
 
 app.use(compression());
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+// connect to database
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
   useFindAndModify: false
 });
 
-// routes
+// api route
 app.use(require("./routes/api.js"));
+// html route
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "./public/index.html"));
+});
+
 
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+  console.log(`🌍 App listening on http://localhost:${PORT}`);
 });
